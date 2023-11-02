d834dfd67737dc85325d29da805882484dc26642
Keshav Agrawal <59056315+Tekionix@users.noreply.github.com>
2023-11-02 12:49:03 +0530

    Add files via upload

---
+++ text_based_adventure_game.py
@@ -0,0 +1,7 @@
+# Text based Adventure Game
+
+def start_adventure():
+    hero_name = input("Enter your hero's name : ")
+    print(f"Welcome {hero_name}, You embark on an epic adventure")
+
+start_adventure()
